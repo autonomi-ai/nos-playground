e3966a4dafff85d77d4f2379b3e966d1bbf81506
jiexiong2016 <jiexiong@kth.se>
2023-12-10 23:47:48 +0100

    add music-gen model

---
+++ examples/music-gen/models/model.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+
+import torch
+from transformers import pipeline
+
+from nos.hub import HuggingFaceHubConfig
+
+@dataclass(frozen=True)
+class MusicGenConfig(HuggingFaceHubConfig):
+    torch_dtype: str = "float16"
+    """Torch dtype string to use for inference."""
+
+class MusicGen:
+    configs = {
+        "facebook/musicgen-small": MusicGenConfig(
+            model_name="facebook/musicgen-small",
+            torch_dtype="float16",
+        ),
+        "facebook/musicgen-medium": MusicGenConfig(
+            model_name="facebook/musicgen-medium",
+            torch_dtype="float16",
+        ),
+        "facebook/musicgen-large": MusicGenConfig(
+            model_name="facebook/musicgen-large",
+            torch_dtype="float16",
+        ),
+    }
+
+    def __init__(self, model_name: str = "facebook/musicgen-small"):
+        try:
+            self.cfg = MusicGen.configs[model_name]
+        except KeyError:
+            raise ValueError(f"Invalid model_name: {model_name}, available models: {Llama2ChatConfig.configs.keys()}")
+
+        if torch.cuda.is_available():
+            self.device_str = "cuda"
+        else:
+            self.device_str = "cpu"
+        self.device = torch.device(self.device_str)
+
+        self.pipe = pipeline("text-to-audio", self.cfg.model_name, device=self.device_str)
+        self.pipe.model.to(self.device)
+    
+    def __call__(self, prompt: str):
+        return self.pipe(prompt, forward_params={"do_sample": True})    
---
+++ examples/music-gen/tests/test.py
@@ -0,0 +1,23 @@
+from typing import List
+from PIL import Image
+from nos.client import Client
+
+def test_music_gen():
+    client = Client("[::]:50051")
+    assert client is not None
+    assert client.WaitForServer()
+    assert client.IsHealthy()
+
+    model_id = "music-gen"
+    models: List[str] = client.ListModels()
+    assert model_id in models
+
+    model = client.Module(model_id)
+    assert model is not None
+    assert model.GetModelInfo() is not None
+    print(f"Test [model={model_id}]")
+
+    response = model(prompt="lo-fi music, chill beats to relax")
+    assert isinstance(response, dict)
+    assert response["audio"] is not None
+    assert response["sampling_rate"] is not None
