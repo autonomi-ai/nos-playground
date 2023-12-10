c7330801e0a5d049e51888b63f87beb58b181dbd
jiexiong2016 <jiexiong@kth.se>
2023-12-10 22:08:09 +0100

    Add comments.

--- examples/animate-diff/models/model.py
+++ examples/animate-diff/models/model.py
@@ -7,13 +7,16 @@ from diffusers import MotionAdapter, AnimateDiffPipeline, DDIMScheduler
 class AnimateDiff():
 
     def __init__(self, model_name: str = "Lykon/dreamshaper-7"):
-
-        # Only support cuda and fp16 for video generation
+        # Assert that CUDA is available for GPU acceleration
         assert torch.cuda.is_available()
 
+        # Initialize the motion adapter from a pretrained model
         adapter = MotionAdapter.from_pretrained("guoyww/animatediff-motion-adapter-v1-5-2", use_safetensors=True)
+        
+        # Initialize the pipeline from a pretrained model and attach the motion adapter
         self.pipe = AnimateDiffPipeline.from_pretrained(model_name, motion_adapter=adapter, use_safetensors=True)
         
+        # Initialize the scheduler from a pretrained model with specified parameters
         scheduler = DDIMScheduler.from_pretrained(
             model_name,
             subfolder="scheduler",
@@ -22,10 +25,12 @@ class AnimateDiff():
             timestep_spacing="linspace",
             steps_offset=1
         )
-
         self.pipe.scheduler = scheduler
+        
+        # Move the pipeline to the GPU and set the data type to float16 for efficiency
         self.pipe.to(torch_device="cuda", torch_dtype=torch.float16)
 
+        # Enable memory saving features in the pipeline
         self.pipe.enable_vae_slicing()
         self.pipe.enable_model_cpu_offload()
 
@@ -42,17 +47,22 @@ class AnimateDiff():
     ) -> List[Image.Image]:
         """Generate animated images from text prompt."""
 
+        # Convert single string prompts to list of strings
         if isinstance(prompts, str):
             prompts = [prompts]
         if isinstance(negative_prompts, str):
             negative_prompts = [negative_prompts]
 
+        # Initialize a random number generator on the GPU
         g = torch.Generator(device="cuda")
+        
+        # Set the seed for the random number generator if specified
         if seed != -1:
             g.manual_seed(seed)
         else:
             g.seed()
 
+        # Generate the animated images
         yield from self.pipe(
             prompt=prompts,
             negative_prompt=negative_prompts,
